604231ea2589ccc0b940da49e62071165b1a3524
hasanshanto01 <hasanshanto01@gmail.com>
2024-10-17 11:27:43 +0600

    making nav menu responsibe by adding sidebar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log("hello from js");
+// elements
+const sidebarElement = document.querySelector(".sidebar");
+const sidebarOpener = document.querySelector("#sidebar-opener");
+
+let isSidebarOpen = false; // state of sidebar
+
+// toggle sidebar
+const handleSidebar = () => {
+  if (isSidebarOpen) {
+    sidebarElement.style.right = "-100%";
+  } else {
+    sidebarElement.style.right = "0";
+  }
+
+  isSidebarOpen = !isSidebarOpen;
+};
+
+// Close sidebar onclick outside
+document.addEventListener("click", (e) => {
+  if (
+    isSidebarOpen &&
+    !sidebarElement?.contains(e.target) &&
+    !sidebarOpener?.contains(e.target)
+  ) {
+    sidebarElement.style.right = "-100%";
+    isSidebarOpen = false;
+  }
+});
