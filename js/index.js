c233b8064a4d3c14398c7a99ace969e8191a9e45
hasanshanto01 <hasanshanto01@gmail.com>
2024-10-18 17:06:01 +0600

    book add, remove to wishlist functionality add

--- js/index.js
+++ js/index.js
@@ -67,10 +67,16 @@ searchInput.addEventListener(
 
 // display content
 const displayContent = (data) => {
-  booksContainer.innerHTML = "";
-  btnContainer.innerHTML = "";
+  spinnerContainer.style.display = "none";
 
   if (data?.count > 0) {
+    notFoundMsg.style.display = "none";
+    booksContainer.style.display = "grid";
+    btnContainer.style.display = "flex";
+
+    booksContainer.innerHTML = "";
+    btnContainer.innerHTML = "";
+
     // books
     data?.results?.forEach((item) => {
       const divElement = document.createElement("div");
@@ -78,6 +84,36 @@ const displayContent = (data) => {
       divElement.setAttribute("data-aos", "fade-up");
       divElement.setAttribute("data-aos-duration", "1000");
       divElement.classList.add("book-card-outer");
+
+      //   const content = `
+      //   <div class="book-card-inner">
+      //     <img
+      //       src=${item?.formats?.["image/jpeg"]}
+      //       alt="Book Cover"
+      //       class="book-img"
+      //     />
+      //     <div class="overlay">
+      //       <div class="overlay-content">
+      //         <div class="content-top">
+      //           <p>ID: ${item?.id || "N/A"}</p>
+      //           <i class="fa-solid fa-heart"></i>
+      //         </div>
+      //         <h4 class="book-title">
+      //            ${item?.title || "Title Unknown"}
+      //         </h4>
+      //         <div class="book-info">
+      //           <p>Genre: ${item?.subjects?.[0] || "Unknown"}</p>
+      //           <p>Author: ${item?.authors?.[0]?.name || "Unknown"}</p>
+      //         </div>
+      //         <div class="details-btn-container">
+      //           <a href="${
+      //             item?.formats?.["text/html"]
+      //           }" target="_blank" class="btn-details">Details</a>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      // `;
 
       const content = `
       <div class="book-card-inner">
@@ -111,6 +147,12 @@ const displayContent = (data) => {
 
       divElement.innerHTML = content;
       booksContainer.appendChild(divElement);
+
+      const heartIcon = divElement.querySelector(".fa-heart");
+      heartIcon.addEventListener("click", (e) => {
+        heartIcon.classList.toggle("liked");
+        handleWishlist(item);
+      });
     });
 
     // previous btn
@@ -150,15 +192,9 @@ const displayContent = (data) => {
     });
 
     btnContainer.appendChild(nextBtnElement);
-
-    notFoundMsg.style.display = "none";
-    booksContainer.style.display = "grid";
-    btnContainer.style.display = "flex";
   } else {
     notFoundMsg.style.display = "block";
   }
-
-  spinnerContainer.style.display = "none";
 };
 
 // handle books fetching
@@ -178,6 +214,19 @@ const handleBooksFetch = (url) => {
       }
     })
     .catch((err) => console.log("Books data fetching error:", err));
+};
+
+// handle wishlist
+const handleWishlist = (data) => {
+  const likedItems = JSON.parse(localStorage.getItem("liked-items")) || [];
+  const itemIndex = likedItems?.findIndex((item) => item?.id === data?.id);
+  if (itemIndex === -1) {
+    likedItems.push(data);
+  } else {
+    likedItems?.splice(itemIndex, 1);
+  }
+  localStorage.setItem("liked-items", JSON.stringify(likedItems));
+  console.log("likedItems:", likedItems);
 };
 
 // initial books load
